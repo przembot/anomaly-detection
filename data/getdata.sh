d32179d05c08f79a4201472a175360a005360be3
Przemysław Kopański <P.Kopanski@stud.elka.pw.edu.pl>
2018-03-28 23:17:05 +0200

    Data download script

---
+++ data/getdata.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# KDD Cup
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/kddcup99-mld/kddcup.names
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/kddcup99-mld/kddcup.data_10_percent.gz
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/kddcup99-mld/kddcup.testdata.unlabeled_10_percent.gz
+gunzip *.gz
+
+# Phishing Websites
+wget -O PW.arff https://archive.ics.uci.edu/ml/machine-learning-databases/00327/Training%20Dataset.arff
+
+# SPECT Heart
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/spect/SPECT.names
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/spect/SPECT.train
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/spect/SPECT.test
