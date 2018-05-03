6605d68bf30e30330c1a5d07170ff8ea92d8e803
KamilaLis <kamila.lis@outlook.com>
2018-05-03 10:33:55 +0000

    testing Random Forest

--- src/rf.R
+++ src/rf.R
@@ -4,6 +4,8 @@ library(caret)
 
 # Data Preparation
 source("src/load.R")
+
+## SPECT
 str(spectTrain)
 
 # Make class variable as a factor (categorical)
@@ -26,8 +28,60 @@ plot(model)
 summary(model)
 
 # Predict using the model
-spectTrain$pred_randomforest<-predict(model,spectTrain)
+spectTest$pred_randomforest<-predict(model,spectTest)
 
 # Accuracy of the model
-confusionMatrix(data = spectTrain$pred_randomforest,
-                reference = spectTrain$V1)
+confusionMatrix(data = spectTest$pred_randomforest,
+                reference = as.factor(spectTest$V1))
+quality <- mean(spectTest$pred_randomforest == spectTest$V1)
+# train: 0.9375
+# test: 0.7754011
+
+
+## PHISHING
+pwebsites$Result = as.factor(pwebsites$Result)
+trainSet <- pwebsitesTrain
+testSet <- pwebsitesTest
+
+# Build the model
+model<-randomForest(Result ~ .,
+                    data=trainSet,
+                    ntree=500,
+                    importance=T)
+plot(model)
+summary(model)
+
+# Predict using the model
+testSet$pred_randomforest<-predict(model,testSet)
+
+# Accuracy of the model
+confusionMatrix(data = testSet$pred_randomforest,
+                reference = as.factor(testSet$Result))
+quality <- mean(testSet$pred_randomforest == testSet$Result)
+# train: 0.9803709
+# test: 0.9604655
+
+
+## KDD CUP
+trainSet <- kddcup
+testSet <- kddcupTest
+trainSet$V42 = as.factor(trainSet$V42)
+testSet$V42 = as.factor(testSet$V42)
+
+# Build the model
+model<-randomForest(V42 ~ .,
+                    data=trainSet,
+                    ntree=500,
+                    importance=T)
+plot(model)
+summary(model)
+
+# Predict using the model
+testSet$pred_randomforest<-predict(model,testSet)
+
+# Accuracy of the model
+confusionMatrix(data = testSet$pred_randomforest,
+                reference = testSet$V42)
+quality <- mean(testSet$pred_randomforest == testSet$V42)
+# train: 
+# test: 
